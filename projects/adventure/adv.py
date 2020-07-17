6c982dc16a6c2f629a5c52f6b30eaf8c0e06d6e5
Juana Villalpando <juana.v14@gmail.com>
2020-07-17 11:02:50 -0700

    MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -23,8 +23,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -42,69 +42,29 @@ player = Player(world.starting_room)
 # path we want to print
 traversal_path = []
 #variable equal to current_room of player when going through walkthrough
-current_room = player.current_room
+start_room = player.current_room
+dfs_stack = []
+visited = {}
 
+while len(visited.keys()) < len(world.rooms):
+    nodes_to_traverse = []
+    current_room = player.current_room
+    for dir in current_room.get_exits():
+        if current_room.get_room_in_direction(dir) not in visited:
+            nodes_to_traverse.append(dir)
 
-room_to_path_taken = {current_room: []}
-stack = []
-for dir in player.current_room.get_exits():
-    next_room = current_room.get_room_in_direction(dir)
-    stack.append((next_room, [dir]))
+    visited[current_room] = True
 
-while stack:
-    next_room, path_taken = stack.pop()
-    if next_room not in room_to_path_taken:
-        current_room = player.current_room
-        # print(current_room.id)
-        travel_to_next_room = False
-        for dir in current_room.get_exits():
-            adjacent_room = current_room.get_room_in_direction(dir)
-            if adjacent_room == next_room:
-                player.travel(dir)
-                traversal_path.append(dir)
-                travel_to_next_room = True
-                break
-            for dir2 in adjacent_room.get_exits():
-                if adjacent_room.get_room_in_direction(dir2) == next_room:
-                    player.travel(dir)
-                    player.travel(dir2)
-                    traversal_path.extend([dir, dir2])
-                    travel_to_next_room = True
-
-        
-        if not travel_to_next_room:
-            path_to_next_node = []
-            path_found = False
-            for dir in traversal_path[::-1]:
-                inversed_dir = inverse(dir)
-                path_to_next_node.append(inversed_dir)
-                player.travel(inversed_dir)
-                current_room = player.current_room
-                for dir2 in current_room.get_exits():
-                    if current_room.get_room_in_direction(dir2) == next_room:
-                        path_to_next_node.append(dir2)
-                        player.travel(dir2)
-                        path_found = True
-                        break
-                if path_found:
-                    break
-            traversal_path.extend(path_to_next_node)
-
-
-
-            # inverse_path = [inverse(dir) for dir in room_to_path_taken[current_room]]
-            # directions = inverse_path[::-1] + path_taken
-            # traversal_path.extend(directions)
-            # for dir in directions:
-            #     player.travel(dir)
-        
-        # print(traversal_path)
-        room_to_path_taken[next_room] = path_taken
-        for dir in next_room.get_exits():
-            next = next_room.get_room_in_direction(dir)
-            stack.append((next, path_taken + [dir]))
-            next_room.connect_rooms(dir, next)
-
+    if nodes_to_traverse:
+        next_move = random.randint(0, len(nodes_to_traverse) - 1)
+        dfs_stack.append(nodes_to_traverse[next_move])
+        player.travel(nodes_to_traverse[next_move])
+        traversal_path.append(nodes_to_traverse[next_move])
+    else:
+        last_seen = dfs_stack.pop()
+        inversed_dir = inverse(last_seen)
+        player.travel(inversed_dir)
+        traversal_path.append(inversed_dir)
 #Queue
 #at each room, check for directions an in 
 #four rooms you see in four directions, see relationship
