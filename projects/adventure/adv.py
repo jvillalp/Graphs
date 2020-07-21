5447d30ac3238ba17ff172b7be8394178be6c43e
Juana Villalpando <juana.v14@gmail.com>
2020-07-20 21:22:36 -0700

    updated

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -39,33 +39,8 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 
 
-# path we want to print
-traversal_path = []
-#variable equal to current_room of player when going through walkthrough
-start_room = player.current_room
-dfs_stack = []
-visited = {}
-
-while len(visited.keys()) < len(world.rooms):
-    nodes_to_traverse = []
-    current_room = player.current_room
-    for dir in current_room.get_exits():
-        if current_room.get_room_in_direction(dir) not in visited:
-            nodes_to_traverse.append(dir)
-
-    visited[current_room] = True
-
-    if nodes_to_traverse:
-        next_move = random.randint(0, len(nodes_to_traverse) - 1)
-        dfs_stack.append(nodes_to_traverse[next_move])
-        player.travel(nodes_to_traverse[next_move])
-        traversal_path.append(nodes_to_traverse[next_move])
-    else:
-        last_seen = dfs_stack.pop()
-        inversed_dir = inverse(last_seen)
-        player.travel(inversed_dir)
-        traversal_path.append(inversed_dir)
-#Queue
+#Understand, Plan
+#Stack
 #at each room, check for directions an in 
 #four rooms you see in four directions, see relationship
 #room south, goes to room 4...
@@ -75,6 +50,48 @@ while len(visited.keys()) < len(world.rooms):
 # print(traversal_path)
 
 
+# path we want to print
+traversal_path = []
+# keep a stack for DFS, and a dict of visited rooms.
+dfs_stack = []
+visited = {}
+
+# DFS. If we hit a dead end (no more nodes_to_traverse), keep going back until
+# we can explore another path.
+while len(visited.keys()) < len(world.rooms):
+    # nodes we can directly traverse to from current node.
+    nodes_to_traverse = []
+    current_room = player.current_room
+    # for valid dir
+    for dir in current_room.get_exits():
+        if current_room.get_room_in_direction(dir) not in visited:
+            # if node has not been visited before, add it as a node we can directly traverse to.
+            nodes_to_traverse.append(dir)
+
+    # Visit the current room.
+    visited[current_room] = True
+
+    # if there are nodes to traverse from here, randomly choose one,
+    # travel there, and add to traversalpath.
+    if nodes_to_traverse:
+        next_move = random.randint(0, len(nodes_to_traverse) - 1)
+        #append next node to traverse-to to the stack
+        dfs_stack.append(nodes_to_traverse[next_move])
+        #move player to that random node needed to traverse
+        player.travel(nodes_to_traverse[next_move])
+        #append next_move of node to the traversal_path which is what we will want counted for moves
+        traversal_path.append(nodes_to_traverse[next_move])
+    # else pop from stack and go backwards until we find a node that has nodes
+    # it can directly traverse to.
+    else:
+        #pop from stoack
+        last_seen = dfs_stack.pop()
+        #go in inverse dir until we find a node we can traverse to
+        inversed_dir = inverse(last_seen)
+        #move player
+        player.travel(inversed_dir)
+        #append to traversal path
+        traversal_path.append(inversed_dir)
 
 # TRAVERSAL TEST
 visited_rooms = set()
